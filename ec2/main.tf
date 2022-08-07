8c5a690aa21655e755d72dfa90a94f5397dfd037
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-08-07 20:58:05 +0530

    Create main.tf

---
+++ ec2/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-08d70e59c07c61a3a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "sampleappserver"
+  }
+}
+output "instance_name" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.app_server.id
+}
